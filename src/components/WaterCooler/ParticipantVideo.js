9e5cb187341a748301bb30c9b2d423fa856175fb
Katie Gray <katie.gray@dolby.com>
2021-11-01 13:57:02 -0700

    resizing avatar circle video stream

--- src/components/WaterCooler/ParticipantVideo.js
+++ src/components/WaterCooler/ParticipantVideo.js
@@ -1,25 +1,20 @@
 import { useRef, useEffect } from 'react';
 
-
 // Inspired by:
 // https://github.com/dolbyio-samples/dolbyio-web-file-presentation-service/blob/362743a41466d66567fd81ecd52b15f605893925/client/components/attendeeVideo.js#L28
 
-const size = 100; //40
+const size = 40;
 
-export const ParticipantVideo = ({ position, videoStream, isSelf}) => {
+export const ParticipantVideo = ({ position, videoStream, isSelf }) => {
   const ref = useRef();
 
- 
-    // add mirror class to video if current participant
-  let  addFlipClass = (isSelf) ? 'flipped':''
+  // add mirror class to video if current participant
+  let addFlipClass = isSelf ? 'flipped' : '';
 
+  let isSafari = navigator.vendor === 'Apple Computer, Inc.' ? true : false;
 
-  let isSafari = (navigator.vendor === "Apple Computer, Inc.") ? true : false;
-
-
-  let topPosition = (isSafari) ? position[1] + 70 : 0
-  let leftPosition = (isSafari) ? position[0] + 265 : 0
-
+  let topPosition = isSafari ? position[1] + 70 : 0;
+  let leftPosition = isSafari ? position[0] + 265 : 0;
 
   useEffect(() => {
     navigator.attachMediaStream(ref.current, videoStream);
@@ -28,17 +23,17 @@ export const ParticipantVideo = ({ position, videoStream, isSelf}) => {
   return (
     <g
       className="avatar-video"
-      transform={`translate(${position[0] - size / 2},${position[1] - size / 2
-        })`}
+      transform={`translate(${position[0] - size / 2},${
+        position[1] - size / 2
+      })`}
     >
       <foreignObject width={size} height={size}>
-
         <div>
           <video
             class={addFlipClass}
             ref={ref}
             style={{
-              position: "absolute",
+              position: 'absolute',
               top: `${topPosition}px`,
               left: `${leftPosition}px`,
               borderRadius: `${size / 2}px`,
@@ -47,9 +42,7 @@ export const ParticipantVideo = ({ position, videoStream, isSelf}) => {
             autoPlay
             muted
           ></video>
-  
         </div>
-
       </foreignObject>
     </g>
   );
